f47f0c5e1e28394160386942f09dc0efbe9d1fec
Bart Bakker <bbakker@ripe.net>
2025-07-20 16:16:18 +0200

    GET RFC6928 NI objects

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use axum::{Router, routing::get};
+use axum::{Router, extract::Path, http::StatusCode, routing::get};
+use base64::{Engine as _, engine::general_purpose::URL_SAFE_NO_PAD};
+
+async fn named_information(Path((alg, val)): Path<(String, String)>) -> (StatusCode, String) {
+    match URL_SAFE_NO_PAD.decode(val.clone()) {
+        Ok(h) => (
+            StatusCode::OK,
+            format!("{alg} - {val}", alg = alg, val = val),
+        ),
+        Err(e) => (StatusCode::BAD_REQUEST, format!("{e}")),
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, world" }))
+        .route("/.well-known/ni/{alg}/{val}", get(named_information));
 
     let listener = tokio::net::TcpListener::bind("[::]:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
